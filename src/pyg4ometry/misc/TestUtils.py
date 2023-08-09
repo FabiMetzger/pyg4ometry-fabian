bc9bfc8ea4e1bef3e9da186c208a38ce85c96370
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2023-08-10 00:43:40 +0100

    fluka : (test) output file diff

--- src/pyg4ometry/misc/TestUtils.py
+++ src/pyg4ometry/misc/TestUtils.py
@@ -24,9 +24,9 @@ def diffFiles(file1, file2):
 
 
 def compareFilesWithAssert(file1, file2):
-    print("compare")
+    print("compare", file1, file2)
     if file1 is not None and file2 is not None:
         diff = _fc.cmp(file1, file2, shallow=False)
         print("compare", diff)
-        if diff:
+        if not diff:
             assert diffFiles(file1, file2)
